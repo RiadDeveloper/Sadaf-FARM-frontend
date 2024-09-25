2a5fe9d85b4ceafa5941dccffa86fda5d72faf47
Siam-555 <siamshahriarhasan@gmail.com>
2024-09-25 09:00:45 +0600

    Primary header done

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div>
+      <img src="https://i.ibb.co.com/7zqqY1s/image.png" alt="image" />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/NavUser.jsx
@@ -0,0 +1,9 @@
+const NavUser = () => {
+  return (
+    <div>
+      User Log in
+    </div>
+  );
+};
+
+export default NavUser;
